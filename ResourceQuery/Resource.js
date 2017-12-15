4a3ce38726a49ec40e88e32d13c5545c4711ccf7
zjsuperman1987 <250529492@qq.com>
2017-12-15 17:39:40 +0800

    commit

--- ResourceQuery/Resource.js
+++ ResourceQuery/Resource.js
@@ -1,12 +1,63 @@
 $(function(){
-	$('.menu').on('click',function(eve){
-		// $(eve.currentTarget).addClass('menu-flex-item-controlBorder')
-		if(eve.target.nodeName.toLowerCase() == 'span') {
-			$('.menu-flex-item').removeClass('menu-flex-item-controlBorder');
-			$(eve.target.parentNode).addClass('menu-flex-item-controlBorder');
-		}else{
-			$('.menu-flex-item').removeClass('menu-flex-item-controlBorder');
-			$(eve.target).addClass('menu-flex-item-controlBorder');
-		}
+	// ====================================选项卡切换====================================
+	// $('.menu').on('click',function(eve){
+	// 	if(eve.target.nodeName.toLowerCase() == 'span') {
+	// 		$('.menu-flex-item').removeClass('menu-flex-item-controlBorder');
+	// 		$(eve.target.parentNode).addClass('menu-flex-item-controlBorder');
+	// 	}else if (eve.target.nodeName.toLowerCase() == 'i') {
+	// 		$('.menu-flex-item').removeClass('menu-flex-item-controlBorder');
+	// 		$(eve.target.parentNode).addClass('menu-flex-item-controlBorder');
+	// 	}else {
+	// 		$('.menu-flex-item').removeClass('menu-flex-item-controlBorder');
+	// 		$(eve.target).addClass('menu-flex-item-controlBorder');
+	// 	}
+	// });
+
+	$('.menu .menu-flex-item').on('click', function(event){
+		$(this).addClass("menu-flex-item-controlBorder").siblings().removeClass("menu-flex-item-controlBorder");
+		var index = $(this).index();
+		$('.MainWrap>div').eq(index).css('display','block').siblings().css('display','none');
+	})
+	// ==================================楼层点击=====================================
+	$('.buildingWrap>div').on('click',function(){
+		$(this).addClass('grid_row_background').siblings().removeClass('grid_row_background');
+		$(this.parentNode).css('display','none');
+
+		$($('.menu-flex-item')[0]).removeClass("menu-flex-item-controlBorder");
+		$($('.menu-flex-item')[1]).addClass("menu-flex-item-controlBorder");
+		$('.floorContentWrap').css('display','block');
+	});
+
+	$('.resourceContent .resourceStyle').on("click", function(){
+		$(this).addClass("btn_style").siblings().removeClass('btn_style');
+	})
+	$('.resourceOfType .resourceStyle').on("click", function(){
+		$(this).addClass("btn_style").siblings().removeClass('btn_style');
+	})
+
+	$('.leaseState .resourceStyle').on("click", function(){
+		$(this).addClass("btn_style").siblings().removeClass('btn_style');
+	})
+
+	$('.rentandsendState .resourceStyle').on("click", function(){
+		$(this).addClass("btn_style").siblings().removeClass('btn_style');
+	})
+
+	$('.btn_bottom div').on('click', function(){
+		$('.fiftrateContentWrap').css('display','none');
+	})
+
+	$('.floorContentWrap .borderAndPadding').on('click',function(){
+
+	})
+
+	// 楼层跳转
+	$('.floorContentWrap .grid_row_tr').on('click',function() {
+		$(this).addClass('grid_row_background').siblings().removeClass('grid_row_background')
+		$('.floorsDetails').css('display','block');
+	})
+
+	$('.floorsDetails_section').on('click',function(){
+		$('.floorLast').css('display', 'block')
 	})
 })
